9cf93d2d79705edc4c06fc79c8e3e087675d5443
Matthew Love <matthewlove94@gmail.com>
2026-01-11 12:03:01 +0000

    Create project for thisclass example

---
+++ thisclass_typedef/Source/thisclass_typedef/thisclass_typedef.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "thisclass_typedef.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, thisclass_typedef, "thisclass_typedef" );
