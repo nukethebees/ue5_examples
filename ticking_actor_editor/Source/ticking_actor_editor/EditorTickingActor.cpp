918dfe33c02b7f6251f786b62b399db6aba60f42
Matthew Love <matthewlove94@gmail.com>
2026-01-12 18:36:41 +0000

    Add project to show editor ticking actors

---
+++ ticking_actor_editor/Source/ticking_actor_editor/EditorTickingActor.cpp
@@ -0,0 +1,20 @@
+#include "ticking_actor_editor/EditorTickingActor.h"
+
+AEditorTickingActor::AEditorTickingActor() {
+#if WITH_EDITOR
+    PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bStartWithTickEnabled = true;
+#else
+    PrimaryActorTick.bCanEverTick = false;
+#endif
+
+    RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+}
+
+void AEditorTickingActor::Tick(float delta_time) {
+    Super::Tick(delta_time);
+
+#if WITH_EDITOR
+    UE_LOG(LogTemp, Warning, TEXT("I'm ticking in the editor!"));
+#endif
+}
---
+++ ticking_actor_editor/Source/ticking_actor_editor/EditorTickingActor.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "EditorTickingActor.generated.h"
+
+UCLASS()
+class AEditorTickingActor : public AActor {
+    GENERATED_BODY()
+  public:
+    AEditorTickingActor();
+
+    virtual void Tick(float delta_time) override;
+
+#if WITH_EDITOR
+    virtual bool ShouldTickIfViewportsOnly() const { return true; }
+#endif
+};
